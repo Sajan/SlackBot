ce716067c5e2c6ad46ee0285daa434f9bd86c5bd
Greg Dietsche <gdietsche@sajan.com>
2015-08-31 17:30:41 -0500

    quiet mode
    Signed-off-by: Greg Dietsche <gdietsche@sajan.com>

--- main.go
+++ main.go
@@ -3,43 +3,63 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/bluele/slack"
 )
 
+var fQuiet = flag.Bool("quiet", false, "Exit and set an error code, but do not print an error message on the console.")
 var fToken = flag.String("token", "", "slack authentication token")
 var fChannel = flag.String("channel", "", "slack channel name")
 var fMessage = flag.String("message", "", "message")
 
-func initFlags() bool {
+const (
+	ErrorNone = iota
+	ErrorPanic
+	ErrorTokenEmpty
+	ErrorChannelEmpty
+	ErrorMessageEmpty
+)
+
+func ErrorExitParm(message string, returnCode int) {
+	if !*fQuiet {
+		fmt.Println(message)
+	}
+	os.Exit(returnCode)
+}
+
+func ErrorExitPanic(err error) {
+	if !*fQuiet {
+		panic(err)
+	} else {
+		os.Exit(ErrorPanic)
+	}
+}
+
+func initFlags() {
 	flag.Parse()
+
 	if *fToken == "" {
-		fmt.Println("-token is a required parameter.")
-		return false
+		ErrorExitParm("-token is a required parameter.", ErrorTokenEmpty)
+	} else if *fChannel == "" {
+		ErrorExitParm("-channel is a required parameter.", ErrorChannelEmpty)
+	} else if *fMessage == "" {
+		ErrorExitParm("-message is a required parameter.", ErrorMessageEmpty)
 	}
-	if *fChannel == "" {
-		fmt.Println("-channel is a required parameter.")
-		return false
-	}
-	if *fMessage == "" {
-		fmt.Println("-message is a required parameter.")
-		return false
-	}
-	return true
 }
 
 func main() {
-	if !initFlags() {
-		return
-	}
+	initFlags()
 
 	api := slack.New(*fToken)
+
 	channel, err := api.FindChannelByName(*fChannel)
 	if err != nil {
-		panic(err)
+		ErrorExitPanic(err)
 	}
+
 	err = api.ChatPostMessage(channel.Id, *fMessage, nil)
 	if err != nil {
-		panic(err)
+		ErrorExitPanic(err)
 	}
 }
